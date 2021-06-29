1be1eb43efc3fb885983e1fb77e25a1384e4ccae
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-06-29 13:13:14 +0200

    Complete first test task

--- KotlinHeroesEpisode7/src/main/kotlin/main.kt
+++ KotlinHeroesEpisode7/src/main/kotlin/main.kt
@@ -5,7 +5,17 @@ private fun readInts() = readStrings().map { it.toInt() } // list of ints
 private fun readPair() = readStrings().take(2)
 private fun readIntPair() = readStrings().take(2).map { it.toInt() }
 
-
 fun main(args: Array<String>) {
-    println("Hello World!")
+    aPlusBTestTask()
+}
+
+private fun aPlusBTestTask() {
+    val n = readInt()
+
+    val ans = StringBuilder()
+    for (i in 0 until n) {
+        val (a, b) = readIntPair()
+        ans.append("${a + b}\n")
+    }
+    print(ans)
 }
