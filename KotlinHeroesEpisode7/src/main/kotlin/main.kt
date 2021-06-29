50832995f12a5f23debcfffbc039f914f4a2c48b
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-06-29 13:24:44 +0200

    Jumping frogs done

--- KotlinHeroesEpisode7/src/main/kotlin/main.kt
+++ KotlinHeroesEpisode7/src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
+import java.lang.StringBuilder
+
 private fun readLn() = readLine()!! // string line
 private fun readInt() = readLn().toInt() // single int
 private fun readStrings() = readLn().split(" ") // list of strings
@@ -6,5 +8,20 @@ private fun readLongs() = readStrings().map { it.toLong() } // list of longs
 
 
 fun main(args: Array<String>) {
-    println("Hello World!")
+    jumpingFrogs()
+}
+
+private fun jumpingFrogs() {
+    val n = readInt()
+
+    val ans = StringBuilder()
+    for (i in 0 until n) {
+        val (a, b, k) = readLongs()
+        val rightJumps = k / 2 + k % 2
+        val leftJumps = k / 2
+        val currentAns = rightJumps * a - leftJumps * b
+        ans.append(currentAns.toString() + '\n')
+    }
+    print(ans)
+
 }
