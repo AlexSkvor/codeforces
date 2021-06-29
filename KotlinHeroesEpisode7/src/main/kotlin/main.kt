b99212ab146a9ecc67e47f2539b72599d79d0aa1
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-06-29 13:20:19 +0200

    Add readLongs

--- KotlinHeroesEpisode7/src/main/kotlin/main.kt
+++ KotlinHeroesEpisode7/src/main/kotlin/main.kt
@@ -2,8 +2,7 @@ private fun readLn() = readLine()!! // string line
 private fun readInt() = readLn().toInt() // single int
 private fun readStrings() = readLn().split(" ") // list of strings
 private fun readInts() = readStrings().map { it.toInt() } // list of ints
-private fun readPair() = readStrings().take(2)
-private fun readIntPair() = readStrings().take(2).map { it.toInt() }
+private fun readLongs() = readStrings().map { it.toLong() } // list of longs
 
 
 fun main(args: Array<String>) {
