07839a40e139a442a579ed720dd91643419ae3c3
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-06-29 17:18:17 +0200

    Sweets completed

--- KotlinHeroesEpisode7/src/main/kotlin/main.kt
+++ KotlinHeroesEpisode7/src/main/kotlin/main.kt
@@ -16,7 +16,41 @@ private fun taskName() {
 
     val ans = StringBuilder()
     repeat(tasksNumber) { taskNumber ->
+        val (sweetNumber, step) = readInts()
+        val loves = readLn().toList().map { it == '1' } // true -> loves
+        val sweets = BooleanArray(sweetNumber) // false -> not eaten!
 
+        var lovesCount = loves.count { it }
+
+        if (lovesCount > 0) {
+
+            var lastEatenIndex = 0
+            var sweetsEaten = 1
+            sweets[0] = true
+            if (loves[0]) lovesCount--
+
+            while (lovesCount > 0) {
+                var stepCounter = 0
+                var index = lastEatenIndex
+                while (stepCounter < step) {
+                    if (!sweets[(index + 1) % sweets.size]) {
+                        index++
+                        stepCounter++
+                    } else {
+                        index++
+                    }
+                }
+                if (loves[index % sweets.size]) {
+                    lovesCount--
+                }
+                sweetsEaten++
+                sweets[index % sweets.size] = true
+                lastEatenIndex = index
+            }
+            ans.append(sweetsEaten.toString() + "\n")
+        } else {
+            ans.append("0\n")
+        }
     }
     print(ans)
 }
