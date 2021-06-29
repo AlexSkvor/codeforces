7125e03946b68423536a3812e776b9946d990aa6
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-06-29 13:07:35 +0200

    Add base methods

--- KotlinHeroesEpisode7/src/main/kotlin/main.kt
+++ KotlinHeroesEpisode7/src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
+private fun readLn() = readLine()!! // string line
+private fun readInt() = readLn().toInt() // single int
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+private fun readPair() = readStrings().take(2)
+private fun readIntPair() = readStrings().take(2).map { it.toInt() }
+
+
 fun main(args: Array<String>) {
     println("Hello World!")
 }
