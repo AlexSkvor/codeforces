f913b155c0750357b28e38088def7854c333c94f
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-06-29 15:28:11 +0200

    Complete team building task

--- KotlinHeroesEpisode7/src/main/kotlin/main.kt
+++ KotlinHeroesEpisode7/src/main/kotlin/main.kt
@@ -1,4 +1,4 @@
-import java.lang.StringBuilder
+import kotlin.math.absoluteValue
 
 private fun readLn() = readLine()!! // string line
 private fun readInt() = readLn().toInt() // single int
@@ -12,11 +12,36 @@ fun main(args: Array<String>) {
 }
 
 private fun taskName() {
-    val tasksNumber = readInt()
+    readInt()
 
-    val ans = StringBuilder()
-    repeat(tasksNumber) { taskNumber ->
+    val studentsSorted = readInts().sorted().toMutableList()
 
+    var ans = 0
+    while (studentsSorted.isNotEmpty()) {
+        val pair = studentsSorted.getMinDiffPair()
+        ans += (studentsSorted[pair.first] - studentsSorted[pair.second]).absoluteValue
+        studentsSorted.removeAt(pair.second)
+        studentsSorted.removeAt(pair.first)
     }
-    print(ans)
+
+    print(ans.toString())
+}
+
+private fun List<Int>.getMinDiffPair(): Pair<Int, Int> {
+    var minDiff = 100
+    var lastIndex = 0
+
+    forEachIndexed { i, a ->
+        if (i > 0) {
+            if (this[i - 1] - a == 0) {
+                return i - 1 to i
+            } else {
+                if (a - this[i - 1] < minDiff) {
+                    lastIndex = i - 1
+                    minDiff = this[i - 1] - a
+                }
+            }
+        }
+    }
+    return lastIndex to (lastIndex + 1)
 }
